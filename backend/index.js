e24735bee8d1ffe60cf040d24cadf8a6e0013416
Ashwini Tripathi <141494737+AshwinTripathi2004@users.noreply.github.com>
2025-07-16 12:56:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5176"],
+ origin:["https://vyomart-frontend.onrender.com" , "http://localhost:5176"],
  credentials:true
 }))
 
